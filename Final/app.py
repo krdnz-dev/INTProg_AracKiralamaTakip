b17bc45941778a9fb1852d896370c80ead432c13
krdnz-dev <polat.karadeniz61@hotmail.com>
2025-05-25 14:25:43 +0300

    app.py

--- Final/app.py
+++ Final/app.py
@@ -379,5 +379,9 @@ def arac_durum_guncelle(id):
     return redirect("/admin")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
+    
+import os
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
